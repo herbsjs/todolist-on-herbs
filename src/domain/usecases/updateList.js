1e15a1b9e37982a589d93afdec99391c3beefc8c
Maik Moura <mm@vortx.com.br>
2020-05-04 14:23:12 -0300

    Inclusão Mutation UpdateList e classe de teste do update list

--- src/domain/usecases/updateList.js
+++ src/domain/usecases/updateList.js
@@ -9,7 +9,7 @@ module.exports.updateList = injection =>
   usecase('Update Todo List', {
     request: { id: Number, name: String },
 
-    authorize: user => (user.canCreateList ? Ok() : Err()),
+    authorize: user => (user.canUpdateList ? Ok() : Err()),
 
     setup: ctx => (ctx.di = Object.assign({}, dependency, injection)),
 
