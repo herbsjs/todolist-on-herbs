e952f704dc3c6d9df984b29d791c09f647af01c9
jd <jd@vortx.com.br>
2020-05-12 23:24:21 -0300

    Set up the structure for the SPA front end of todo list app

---
+++ ui-react/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from './routes';
+import './App.css';
+import history from './extensions/history';
+
+function App() {
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
+}
+
+export default App;
