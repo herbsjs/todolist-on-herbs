289b5f19d85319e85fb1c5a402593d45e4ef4c15
jd <jd@vortx.com.br>
2020-05-13 09:42:56 -0300

    Revert "Revert "Set up the structure for the SPA front end of todo list app""
    This reverts commit 4050f0eb78c2504be8e0904df2f8e6250c8d67bf.

---
+++ ui-react/src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+  function Header() {
+    return (
+    <>
+    <div className="header">
+        <h1 className="header-title">Keep calm and get things DONE!</h1>
+    </div>
+    </>
+    )
+}
+
+export default Header;
---
+++ ui-react/src/pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import Form from './Form/Form';
+import Formlist from './Formlist/Formlist';
+
+  function Home() {
+    return (
+    <>
+        <Form/>
+        <Formlist/>
+    </>
+    )
+}
+
+export default Home;
