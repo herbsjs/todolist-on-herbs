e0d2ebb5f3e6a810826878c4978325a304fb499c
VORTX\mbd <mbd@vortx.com.br>
2020-06-09 00:53:22 -0300

    Add canUpdateList and remove eslit configs

--- backend/src/infra/api/graphql/resolvers/mutation/updateList.js
+++ backend/src/infra/api/graphql/resolvers/mutation/updateList.js
@@ -9,7 +9,7 @@ const resolvers = {
     updateList: async (parent, args) => {
       const di = Object.assign({}, dependency, args.injection)
       const uc = di.updateList(args.injection)
-      const hasAccess = uc.authorize({ canCreateList: true }) // TODO: authorize user
+      const hasAccess = uc.authorize({ canUpdateList: true }) // TODO: authorize user
       const response = await uc.run({ id: args.id, name: args.name })
 
       if (response.isErr) throw new UserInputError(null, { invalidArgs: response.err })
