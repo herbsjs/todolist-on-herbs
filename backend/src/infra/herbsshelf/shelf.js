a176a2442fdd6bcb1d967be4869decb48d2927fb
Enderson Costa <enderson.tech@gmail.com>
2020-10-25 09:56:46 -0300

    css separation

--- backend/src/infra/herbsshelf/shelf.js
+++ backend/src/infra/herbsshelf/shelf.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const generateShelfData = (usecases) => {
   let shelfData = []
   for (const [ucsGroup, ucs] of Object.entries(usecases)) {
@@ -10,6 +13,11 @@ const generateShelfData = (usecases) => {
   return shelfData
 }
 
+const getCssStyle = () => {
+  const cssFilePath = path.resolve(__dirname, 'shelf.css');
+  return fs.readFileSync(cssFilePath, "utf-8")
+}
+
 const html = (shelfData) => `
 <!DOCTYPE html>
 <html lang="en">
@@ -17,218 +25,10 @@ const html = (shelfData) => `
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Herbs Shelf</title>
-
-    <style>
-      @import url("https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap");
-
-      /* color definition */
-      :root {
-        --primary: #958b6e;
-        --dark: #3f3f3f;
-        --light: #f1f1f1;
-        --lighter: #fafafa;
-      }
-
-      /* browser default configuration reset */
-      * {
-        padding: 0;
-        margin: 0;
-        -moz-box-sizing: border-box;
-        box-sizing: border-box;
-      }
-
-      a {
-        text-decoration: none;
-      }
-
-      body {
-        background-color: var(--primary);
-        color: var(--dark);
-        font-family: "Open Sans", sans-serif;
-      }
-
-      main {
-        background-color: var(--lighter);
-        max-width: 1200px;
-        min-width: 1200px;
-        margin: 0 auto;
-        min-height: 100vh;
-        position: relative;
-        display: flex;
-      }
-
-      nav {
-        width: 30%;
-        height: 100%;
-        min-height: 100vh;
-        padding-bottom: 50px;
-        position: relative;
-        background-color: var(--light);
-      }
-
-      .contribute {
-        position: fixed;
-        width: calc(1200px * 0.3);
-        padding: 20px;
-        display: block;
-        text-align: center;
-        bottom: 0;
-
-        color: var(--dark);
-        background-color: var(--light);
-      }
-
-      .text-center {
-        text-align: center;
-      }
-
-      .logo {
-        width: 250px;
-        max-width: 100%;
-      }
-
-      section.content {
-        width: 70%;
-        height: 100%;
-        position: relative;
-        padding: 30px;
-      }
-
-      h1,
-      h2,
-      h3 {
-        font-weight: 400;
-      }
-
-      .shelf-title {
-        margin-bottom: 40px;
-        font-size: 48px;
-        font-weight: lighter;
-        text-align: center;
-      }
-
-      .arrow-to-nav {
-        width: 150px;
-        margin-top: 100px;
-        color: var(--primary);
-        animation: arrowMove infinite 2s alternate ease-in-out;
-      }
-
-      @keyframes arrowMove {
-        from {
-          filter: drop-shadow(0px 0px 0px var(--dark));
-          transform: translateX(0px);
-        }
-        to {
-          filter: drop-shadow(18px 0px 0px var(--dark));
-          transform: translateX(50px);
-        }
-      }
-
-      ul.uc-nav,
-      ul.uc-list-nav,
-      ul.steps-list {
-        list-style: none;
-        position: relative;
-        padding: 20px;
-      }
-
-      ul.if-steps-list {
-        list-style: none;
-        position: relative;
-        padding: 0 20px;
-      }
-
-      ul.uc-nav li {
-        margin-bottom: 10px;
-      }
-
-      .icon {
-        position: relative;
-        padding-left: 30px;
-      }
-
-      .i-folder::after {
-        content: "";
-        left: 0;
-        top: 0;
-        position: absolute;
-        width: 24px;
-        height: 24px;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSI0OHB4IiBoZWlnaHQ9IjQ4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNMTAgNEg0Yy0xLjEgMC0xLjk5LjktMS45OSAyTDIgMThjMCAxLjEuOSAyIDIgMmgxNmMxLjEgMCAyLS45IDItMlY4YzAtMS4xLS45LTItMi0yaC04bC0yLTJ6Ii8+PC9zdmc+");
-      }
-
-      .i-folder-open::after {
-        content: "";
-        left: 0;
-        top: 0;
-        position: absolute;
-        width: 24px;
-        height: 24px;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSI0OHB4IiBoZWlnaHQ9IjQ4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNMjAgNmgtOGwtMi0ySDRjLTEuMSAwLTEuOTkuOS0xLjk5IDJMMiAxOGMwIDEuMS45IDIgMiAyaDE2YzEuMSAwIDItLjkgMi0yVjhjMC0xLjEtLjktMi0yLTJ6bTAgMTJINFY4aDE2djEweiIvPjwvc3ZnPg==");
-      }
-
-      .i-code::after {
-        content: "";
-        left: 0;
-        top: 0;
-        position: absolute;
-        width: 24px;
-        height: 24px;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSI0OHB4IiBoZWlnaHQ9IjQ4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMFYweiIgZmlsbD0ibm9uZSIvPjxwYXRoIGQ9Ik05LjQgMTYuNkw0LjggMTJsNC42LTQuNkw4IDZsLTYgNiA2IDYgMS40LTEuNHptNS4yIDBsNC42LTQuNi00LjYtNC42TDE2IDZsNiA2LTYgNi0xLjQtMS40eiIvPjwvc3ZnPg==");
-      }
-
-      .i-play::after {
-        content: "";
-        left: 0;
-        top: 0;
-        position: absolute;
-        width: 24px;
-        height: 24px;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDI0IDI0IiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9ImJsYWNrIiB3aWR0aD0iNDhweCIgaGVpZ2h0PSI0OHB4Ij48Zz48cmVjdCBmaWxsPSJub25lIiBoZWlnaHQ9IjI0IiB3aWR0aD0iMjQiLz48L2c+PGc+PGc+PHJlY3QgaGVpZ2h0PSIyIiB3aWR0aD0iOSIgeD0iMTMiIHk9IjciLz48cmVjdCBoZWlnaHQ9IjIiIHdpZHRoPSI5IiB4PSIxMyIgeT0iMTUiLz48cmVjdCBoZWlnaHQ9IjIiIHdpZHRoPSI2IiB4PSIxNiIgeT0iMTEiLz48cG9seWdvbiBwb2ludHM9IjEzLDEyIDgsNyA4LDExIDIsMTEgMiwxMyA4LDEzIDgsMTciLz48L2c+PC9nPjwvc3ZnPg==");
-      }
-
-      .i-if::after {
-        content: "";
-        left: 0;
-        top: 0;
-        position: absolute;
-        width: 24px;
-        height: 24px;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgZmlsbD0iYmxhY2siIHdpZHRoPSI0OHB4IiBoZWlnaHQ9IjQ4cHgiPjxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz48cGF0aCBkPSJNMTQgNGwyLjI5IDIuMjktMi44OCAyLjg4IDEuNDIgMS40MiAyLjg4LTIuODhMMjAgMTBWNHptLTQgMEg0djZsMi4yOS0yLjI5IDQuNzEgNC43VjIwaDJ2LTguNDFsLTUuMjktNS4zeiIvPjwvc3ZnPg==");
-      }
-
-      .i-then::after {
-        content: "";
-        left: 0;
-        top: 0;
-        position: absolute;
-        width: 24px;
-        height: 24px;
-        background-repeat: no-repeat;
-        background-size: cover;
-        background-image: url("data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIGVuYWJsZS1iYWNrZ3JvdW5kPSJuZXcgMCAwIDI0IDI0IiB2aWV3Qm94PSIwIDAgMjQgMjQiIGZpbGw9ImJsYWNrIiB3aWR0aD0iNDhweCIgaGVpZ2h0PSI0OHB4Ij48Zz48cmVjdCBmaWxsPSJub25lIiBoZWlnaHQ9IjI0IiB3aWR0aD0iMjQiIHg9IjAiLz48L2c+PGc+PGc+PGc+PHBhdGggZD0iTTMsOC40MWw5LDlsNy03VjE1aDJWN2gtOHYyaDQuNTlMMTIsMTQuNTlMNC40MSw3TDMsOC40MXoiLz48L2c+PC9nPjwvZz48L3N2Zz4=");
-      }
-
-      .selected {
-        font-weight: 600;
-      }
-
-      .hidden {
-        display: none;
-      }
-    </style>
   </head>
+  <style>
+    ${getCssStyle()}
+  </style>
   <body>
     <main id="shelf">
       <nav>
