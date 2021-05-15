fd10e22ca920094105198788bd0cd4f2726af5a2
gabrielmellooliveira <gabrielmello3011@gmail.com>
2021-05-15 18:12:36 -0300

    Improvements in the use of herbs2rest

--- backend/src/domain/usecases/_uclist.js
+++ backend/src/domain/usecases/_uclist.js
@@ -4,7 +4,6 @@ module.exports = (injection) => {
         { usecase: require('./updateItem').updateItem(injection), tags: { group: 'Items' } },
         { usecase: require('./createList').createList(injection), tags: { group: 'List' } },
         { usecase: require('./getLists').getLists(injection), tags: { group: 'List' } },
-        { usecase: require('./getList').getList(injection), tags: { group: 'List' } },
         { usecase: require('./updateList').updateList(injection), tags: { group: 'List' } },
         { usecase: require('./deleteList').deleteList(injection), tags: { group: 'List' } },
     ]
--- backend/src/infra/api/rest/controllerList.js
+++ backend/src/infra/api/rest/controllerList.js
@@ -2,6 +2,7 @@ const controllerList = [
   {
     name: 'lists',
     getAll: require('../../../domain/usecases/getLists').getLists,
+    getById: require('../../../domain/usecases/getLists').getLists,
     post: require('../../../domain/usecases/createList').createList,
     put: require('../../../domain/usecases/updateList').updateList,
     delete: require('../../../domain/usecases/deleteList').deleteList
