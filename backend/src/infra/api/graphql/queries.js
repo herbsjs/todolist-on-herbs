491b7815cc04208387203cc67918f80302f267fa
nathalia45 <nath.f.f@hotmail.com>
2021-07-19 16:25:51 -0300

    Fix https://github.com/herbsjs/todolist-on-herbs/pull/61#discussion_r671568539

--- backend/src/infra/api/graphql/queries.js
+++ backend/src/infra/api/graphql/queries.js
@@ -2,8 +2,8 @@ const { usecase2query, defaultResolver } = require('@herbsjs/herbs2gql')
 
 const usecases = [
     require('../../../domain/usecases/getLists').getLists,
-    /* Add more use cases here */
     require('../../../domain/usecases/getItems').getItems
+    /* Add more use cases here */
 ]
 
 const queries = usecases.map(usecase => usecase2query(usecase(), defaultResolver(usecase)))
