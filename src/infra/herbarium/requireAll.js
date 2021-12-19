19ebce5f0176b262c2b3bb1b24073328487edc33
David Lojudice Sb <dalssoft@gmail.com>
2021-12-18 21:15:55 -0300

    avoidFiles

--- src/infra/herbarium/requireAll.js
+++ src/infra/herbarium/requireAll.js
@@ -4,8 +4,8 @@ const reqall = require('require-all')
 module.exports = function requireAll(options = {}) {
     const initialPath = options.initialPath || process.cwd()
     const __dir = initialPath.split(path.sep).join(path.posix.sep)
-    const avoidTestFiles = options.avoidTest || ((fileName) => fileName.includes('test.js') ? false : fileName)
-    reqall({ dirname: __dir + (options.entitiesPath || '/src/domain/entities'), filter: avoidTestFiles })
-    reqall({ dirname: __dir + (options.usecasesPath || '/src/domain/usecases'), filter: avoidTestFiles })
-    reqall({ dirname: __dir + (options.repositoriesPath || '/src/infra/repositories'), filter: avoidTestFiles })
+    const avoidFiles = options.avoidFiles || ((fileName) => fileName.includes('test.js') ? false : fileName)
+    reqall({ dirname: __dir + (options.entitiesPath || '/src/domain/entities'), filter: avoidFiles })
+    reqall({ dirname: __dir + (options.usecasesPath || '/src/domain/usecases'), filter: avoidFiles })
+    reqall({ dirname: __dir + (options.repositoriesPath || '/src/infra/repositories'), filter: avoidFiles })
 }
