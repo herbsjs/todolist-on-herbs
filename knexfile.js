5d18d2b3577a26bdb87ea4442a369834d7779a79
Jhonathan Soares <jhonathanmarolo@hotmail.com>
2022-07-17 22:39:12 -0300

    add sqlite in prod

--- knexfile.js
+++ knexfile.js
@@ -1,14 +1,9 @@
 module.exports = {
 
   development: {
-    client: 'postgresql',
-    connection: {
-      database: 'todolist_on_herbs_db',
-      user: 'postgres',
-      password: 'postgres',
-      host: 'localhost',
-      port: 5432
-    },
+    client: 'sqlite3',
+    connection: { filename: "file.sqlite:memDb1?mode=memory&cache=shared" },
+    useNullAsDefault: true,
     pool: {
       min: 2,
       max: 10
@@ -24,6 +19,21 @@ module.exports = {
 
   staging: {},
 
-  production: {}
+  production: {
+    client: 'sqlite3',
+    connection: { filename: "file.sqlite:memDb1?mode=memory&cache=shared" },
+    useNullAsDefault: true,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './src/infra/db/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './src/infra/db/seeds/dev'
+    }
+  }
 
 }
