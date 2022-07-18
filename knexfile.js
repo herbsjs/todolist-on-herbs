00697bd5ceb4108b2b4212f4e716ab389f89ae27
Jhonathan Soares <jhonathanmarolo@hotmail.com>
2022-07-17 22:46:17 -0300

    change to postgress connection

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,14 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: { filename: "file.sqlite:memDb1?mode=memory&cache=shared" },
-    useNullAsDefault: true,
+    client: 'postgresql',
+    connection: {
+      database: 'todolist_on_herbs_db',
+      user: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      port: 5432
+    },
     pool: {
       min: 2,
       max: 10
