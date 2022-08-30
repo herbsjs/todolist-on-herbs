673ef0d9f5c8f2438c5c12e57505d72684cf8e7f
Matheus Vicente <mvicente@outlook.com.br>
2022-08-29 21:46:59 -0300

    Removed sqllite config from file JSON.

--- src/infra/config/database.js
+++ src/infra/config/database.js
@@ -2,6 +2,9 @@ module.exports = {
   client: process.env.DATABASE_CLIENT,
   database: {
     connection: {
+      useNullAsDefault: true,
+      client: process.env.DATABASE_CLIENT,
+      filename: process.env.DATABASE_CONNECTION,
       host: process.env.DATABASE_HOST,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
