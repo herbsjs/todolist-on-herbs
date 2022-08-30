e843383dc0a6b5289433bfa3433c5f4a32b6d464
Matheus Vicente <mvicente@outlook.com.br>
2022-08-29 21:41:35 -0300

    Updated shelf handler.

--- src/infra/handlers/herbsshelf.js
+++ src/infra/handlers/herbsshelf.js
@@ -1,20 +1,10 @@
-const renderShelfHTML = require('@herbsjs/herbsshelf')
+const { herbsshelf } = require('@herbsjs/herbsshelf')
 const { herbarium } = require('@herbsjs/herbarium')
 
 module.exports = async (event, context, callback) => {
-  herbarium.requireAll()
-
-  const usecases = Array.from(herbarium.usecases.all).map(([_, item]) => ({
-    usecase: item.usecase(),
-    id: item.id,
-    tags: { group: item.group },
-  }))
-
-  const shelf = renderShelfHTML('TODO List', usecases)
-
   return {
     statusCode: 200,
-    body: shelf,
+    body: herbsshelf({ project: 'TODO List', herbarium }),
     headers: { 'Content-Type': 'text/html' },
   }
 }
